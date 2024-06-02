b60ad0d67573005e55fe2358ec9c0d97a1f50ec7
João Victor Olivo <jvso@yahoo.com>
2024-06-02 13:20:10 -0300

    Added custom search methods to Repositories

--- back-end/src/main/java/com/melodify/datasource/repository/MusicRepository.java
+++ back-end/src/main/java/com/melodify/datasource/repository/MusicRepository.java
@@ -4,6 +4,9 @@ import com.melodify.datasource.entity.MusicEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MusicRepository extends JpaRepository<MusicEntity, Long> {
+    Optional<MusicEntity> findByName(String name);
 }
--- back-end/src/main/java/com/melodify/datasource/repository/PlaylistRepository.java
+++ back-end/src/main/java/com/melodify/datasource/repository/PlaylistRepository.java
@@ -4,6 +4,9 @@ import com.melodify.datasource.entity.PlaylistEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlaylistRepository extends JpaRepository<PlaylistEntity, Long> {
+    Optional<PlaylistEntity> findByName(String name);
 }

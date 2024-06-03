75571efd0aedb4ab9a63e32463992fcf7c598a75
João Victor Olivo <jvso@yahoo.com>
2024-06-03 18:14:11 -0300

    Formatted MusicRequest

--- back-end/src/main/java/com/melodify/controller/dto/request/MusicRequest.java
+++ back-end/src/main/java/com/melodify/controller/dto/request/MusicRequest.java
@@ -2,5 +2,12 @@ package com.melodify.controller.dto.request;
 
 import java.io.File;
 
-public record MusicRequest(String name, String artistName, String artistUser, String[] genres, Boolean downloadOnlySelected, File audio) {
+public record MusicRequest(
+        String name,
+        String artistName,
+        String artistUser,
+        String[] genres,
+        Boolean downloadOnlySelected,
+        File audio
+) {
 }

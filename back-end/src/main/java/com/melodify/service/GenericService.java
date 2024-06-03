c21538c11e28c960abdc1d8517318971ef15f39b
João Victor Olivo <jvso@yahoo.com>
2024-06-03 12:20:52 -0300

    created GenericService interface

--- back-end/src/main/java/com/melodify/service/GenericService.java
+++ back-end/src/main/java/com/melodify/service/GenericService.java
@@ -1,60 +1,11 @@
 package com.melodify.service;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import com.melodify.infra.exception.CustomException;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-
 import java.util.List;
 
-@Data
-@RequiredArgsConstructor
-@Slf4j
-public abstract class GenericService<E, DTO, R extends JpaRepository<E, Long>> {
-    private final R repository;
-
-    public E get(Long id) {
-        log.info("GenericService.get({}) -> Buscando entidade por id no repositório", id);
-        return repository.findById(id)
-                .orElseThrow(() -> new CustomException(
-                        HttpStatus.NOT_FOUND, "Entidade não encontrada com ID: " + id));
-    }
-
-    public List<E> getAll() {
-        log.info("GenericService.getAll() -> Buscando todas as entidades no repositório");
-        return repository.findAll();
-    }
-
-    public E create(DTO entity) {
-        log.info("GenericService.create(DTO entity) -> Criando entidade baseado na Requisição dada");
-        log.info("SERVICE equalProperties() -> ( Chamada: create(...) ) Transferindo dados da Requisição para uma Entidade própria");
-        E entitySave = equalProperties(newEntity(), entity);
-        return repository.save(entitySave);
-    }
-
-    public E alter(Long id, DTO data) {
-        log.info("GenericService.alter({}, DTO data) -> Alterando por id baseado na Requisição dada", id);
-        E entityFound = repository.findById(id)
-                .orElseThrow(
-                        () -> new CustomException(
-                                HttpStatus.NOT_FOUND,
-                                "Entidade não encontrada com ID: " + id));
-        log.info("SERVICE equalProperties() -> ( Chamada: alter(...) ) Transferindo dados da Requisição para uma Entidade própria");
-        return repository.save(equalProperties(entityFound, data));
-    }
-
-    public String delete(Long id) {
-        log.info("GenericService.delete({}) -> Deletando entidade com id informado", id);
-        E entity = repository.findById(id)
-                .orElseThrow(() -> new CustomException(
-                        HttpStatus.NOT_FOUND, "Entidade não encontrada com ID: " + id));
-        repository.delete(entity);
-        return "Entidade com ID: "+ id +" deletado com sucesso!";
-    }
-
-    public abstract E equalProperties(E entity, DTO data);
-
-    public abstract E newEntity();
+public interface GenericService<E, DTO> {
+    E get(Long id);
+    List<E> getAll();
+    E create(DTO entity);
+    E alter(Long id, DTO entity);
+    String delete(Long id);
 }

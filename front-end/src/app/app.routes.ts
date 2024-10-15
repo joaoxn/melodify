5a15e159341da41219ec2a76c5cbda89f715bbc7
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-10-15 16:17:47 -0300

    Creation of the Register component and implementation of Bootstrap

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'pages/register', component: RegisterComponent },
+    { path: '',   redirectTo: '/pages/register', pathMatch: 'full' }, 
+];

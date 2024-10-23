c0d1a7bcde1e00fa565e9f4b83745ed700185db0
João <jvso@yahoo.com>
2024-10-23 09:48:03 -0300

    Responsiveness for register

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-    { path: 'pages/register', component: RegisterComponent },
-    { path: '',   redirectTo: '/pages/register', pathMatch: 'full' }, 
+    { path: 'register', component: RegisterComponent },
+    { path: '',   redirectTo: 'register', pathMatch: 'full' }, 
 ];
--- front-end/src/app/pages/register/register.component.ts
+++ front-end/src/app/pages/register/register.component.ts
@@ -1,8 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
@@ -17,11 +17,39 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   styleUrls: ['./register.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
   hidePassword = true;
 
-    togglePasswordVisibility() {
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    })
+  }
+
+  togglePasswordVisibility() {
     this.hidePassword = !this.hidePassword;
   }
   
+  register() {
+    if (this.form.invalid) {
+      console.error('Form is invalid');
+      return;
+    }
+
+    const newUser = {
+      name: this.form.get('name')!.value,
+      email: this.form.get('email')!.value,
+      password: this.form.get('password')!.value
+    }
+
+    // TODO: Implement registration logic here with services
+
+    this.router.navigate(['/home']);
+  }
 }

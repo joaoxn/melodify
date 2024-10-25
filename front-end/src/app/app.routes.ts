1c26142263543a98191bc5c1a3165d234a9b212c
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-10-25 09:04:58 -0300

    Routes config and global styles

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PageComponent } from './shared/components/page/page.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
-    { path: 'home', component: HomeComponent},
     { path: '',   redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: '', component: PageComponent, children: [
+            { path: 'home', component: HomeComponent },
+        ]
+    },
 ];

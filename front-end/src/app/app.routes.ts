ced3a1f89b2032fc6f926b93b2b1fb12c8e3e84e
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-10-26 10:05:58 -0300

    Fix routes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
-import { HomeComponent } from './pages/home/home.component';
 import { PageComponent } from './shared/components/page/page.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },

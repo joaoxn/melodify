09e48bfce827b508acdb0b72c121e790b3c1465a
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-10-26 10:07:40 -0300

    Importing Footer Component in page component

--- front-end/src/app/shared/components/page/page.component.ts
+++ front-end/src/app/shared/components/page/page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })

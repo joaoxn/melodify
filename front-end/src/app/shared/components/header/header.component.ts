1d6d3968d5c9506c64569c6060a08622db8b2fe9
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-10-27 22:08:24 -0300

    Search input customized

--- front-end/src/app/shared/components/header/header.component.ts
+++ front-end/src/app/shared/components/header/header.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })

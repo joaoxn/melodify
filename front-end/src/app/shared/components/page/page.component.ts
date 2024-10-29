531a1f0da4f1c7ce460756eb586ef7891958312a
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-10-29 19:20:24 -0300

    Fix header and page

--- front-end/src/app/shared/components/page/page.component.ts
+++ front-end/src/app/shared/components/page/page.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [RouterOutlet, DigitsPipe],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, SidebarComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })

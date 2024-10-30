b43a085175753fdc5b54c9fc6152ee7c8bdd3c4c
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-10-30 08:03:35 -0300

    Created music-register route

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { RegisterComponent } from './pages/register/register.component';
 import { PageComponent } from './shared/components/page/page.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
+import { MusicRegisterComponent } from './pages/music-register/music-register.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent, canActivate: [authGuard] },
+    { path: 'music-register', component: MusicRegisterComponent, canActivate: [authGuard] },
     { path: '',   redirectTo: 'home', pathMatch: 'full' },
     {
         path: '', component: PageComponent, canActivate: [authGuard], children: [

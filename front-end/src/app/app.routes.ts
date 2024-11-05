159be9b00ef1e5deb35cbf261cbd38af3746d0a7
Miguel Fazio <faziodeassuncaomiguel@gmail.com>
2024-11-04 22:19:42 -0300

    Login route implemented

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageComponent } from './shared/components/page/page.component';
 import { authGuard } from './shared/guards/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent, canActivate: [authGuard] },
+    { path: 'login', component: LoginComponent, canActivate: [authGuard] },
     { path: '',   redirectTo: 'home', pathMatch: 'full' },
     {
         path: '', component: PageComponent, canActivate: [authGuard], children: [
